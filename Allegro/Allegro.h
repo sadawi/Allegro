1998cd34d7ba85836a98d6d66e2d2be0685a13aa
Sam Williams <samuel.williams@gmail.com>
2015-12-04 18:50:02 -0500

    Initial Commit

---
+++ Allegro/Allegro.h
@@ -0,0 +1,19 @@
+//
+//  Allegro.h
+//  Allegro
+//
+//  Created by Sam Williams on 12/4/15.
+//  Copyright © 2015 Sam Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Allegro.
+FOUNDATION_EXPORT double AllegroVersionNumber;
+
+//! Project version string for Allegro.
+FOUNDATION_EXPORT const unsigned char AllegroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Allegro/PublicHeader.h>
+
+
